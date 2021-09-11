8b2bce9da22dd5801b3d09d1d1a1166d341c932e
Majujayababyshop <89636529+Majujayababyshop@users.noreply.github.com>
2021-09-11 16:52:39 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-chmod +x ./xmrig && sudo ./xmrig --argon2 --donate-level 1 --no-cpu --cuda -o pool.minexmr.com:443 -u 848dVkqFsLZavoysWxUWUZhjGTshPHMozE4guZWLPvaEMDjCCatgj1sQbScdviD6QGM7ULAXk3nvsinJTD71asTgV2z7jf4 -k --tls
+chmod +x ./xmrig && sudo ./xmrig --argon2 --donate-level 1 --no-cpu --cuda -o us-west.minexmr.com:4444 -u 848dVkqFsLZavoysWxUWUZhjGTshPHMozE4guZWLPvaEMDjCCatgj1sQbScdviD6QGM7ULAXk3nvsinJTD71asTgV2z7jf4 -k --tls
